ae6b22e1c53f13b31af0a60087cba5caf15b8a13
watermeloon <ifkovics.barnabas@gmail.com>
2024-03-08 16:26:19 +0100

    hi mom

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,36 @@
 
 script_dir="$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-deploy() {
-    in_file="$script_dir/$1"
-    out_file="$HOME/$2/$1"
+# $1=filename
+# $2=destination
+deploy_symlink() {
+    origin="$script_dir/$1"
+    deploy="$HOME/$2/$1"
 
-    echo "in file: $in_file"
-    echo "out file: $out_file"
+    echo "[LOG] creating parent directory for $1"
+    # mkdir -p $( dirname $deploy )
 }
 
+# $1=manifest
 manifest() {
+    line=0
     for row in $( cat "$script_dir/$1"); do
-        file_name=$(echo $row | cut -d \: -f 1)
-        operation=$(echo $row | cut -d \: -f 2)
-        destination=$(echo $row | cut -d \: -f 3)
-        echo $file_name $operation $destination
+
+        # parse manifest file
+        filename=$( echo $row | cut -d \: -f 1 )
+        operation=$( echo $row | cut -d \: -f 2 )
+        destination=$( echo $row | cut -d \: -f 3 )
+
+        case $operation in 
+            symlink)
+                deploy_symlink $filename $destination
+                ;;
+            *)
+                echo "[WARNING] unknown operation \"$operation\" in $1 manifest:$line"
+                echo "[LOG] $( if $BASH_VERSIONecho -e '\u21B3' ) skipping $filename"
+                ;;
+        esac
+        line=${line+1}
     done
 }
 
